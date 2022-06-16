634fa550f7d321e9a6dc4e28b205958cde0303ae
WIDZpy <raphstrug@gmail.com>
2022-06-16 14:49:10 +0200

    Ajoutez des fichiers projet.

---
+++ apretisage du langage Cpp/apretisage du langage Cpp.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // int nb = 0;
+    // float fl = 0.0f;
+    // std::cout << "Hello World?" << std::endl;
+    // std::cin >> nb;
+    // fl = nb / 2;
+    // std::cout << fl;
+    // 
+    // bool condition = true;
+    // int nb = 0;
+    // 
+    // std::cout << "doner un bool";
+    // std::cin >> condition;
+    // std::cin.ignore();
+    // std::cout << "doner un chifre";
+    // std::cin >> nb;
+    // std::cin.ignore();
+    // 
+    // 
+    // if (condition == true)
+    // {
+    //     std::cout << "true";
+    // }
+    // else if (nb != 0)
+    // {
+    //     std::cout << "!= 0";
+    // }
+    // int table = 1;
+    // std::cin >> table;
+    // std::cout << "table de " << table << ":" << std::endl;
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     std::cout << i << " X " << table << " = " << i * table << std::endl;
+    // }
+    // 
+    int compteur = 0;
+    while (true)
+    {
+        for (int i = 0; i < compteur; i++)
+        {
+            printf("raphaël");
+        }
+        printf("\n\n\n");
+
+        std::cout << compteur;
+        
+        compteur++;
+    }
+}
